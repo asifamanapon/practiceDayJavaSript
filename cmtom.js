2223bdfab687ff016cd259c3aab129903f26c99b
asifamanapon <aponasifaman@gmail.com>
2022-09-11 22:12:47 +0600

    second-commit

---
+++ cmtom.js
@@ -0,0 +1,8 @@
+function centimeterToMeter(cmtom) {
+    var meterCount = cmtom / 100;
+    return meterCount;
+
+}
+var numberDao = 200;
+var centemeterCount = centimeterToMeter(numberDao);
+console.log(centemeterCount);
